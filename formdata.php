aa49843f46fa2ffaa87251478a7e9b072318a04d
1084road8 <60108867+1084road8@users.noreply.github.com>
2020-02-11 10:53:20 -0700

    Create formdata.php

---
+++ formdata.php
@@ -0,0 +1,12 @@
+<html>
+<body>
+
+Name:<?php echo $_POST["name"]; ?><br>
+Phone:<?php echo $_POST["phone"]; ?>
+Email:<?php echo $_POST["email"]; ?><br>
+Message:<?php echo $_POST["message"]; ?>
+
+
+
+</body>
+</html>
